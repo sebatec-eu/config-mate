513944ebecfc6fb1fdd0c07f108e860a56624e23
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2025-03-24 16:39:49 +0100

    Add documentation

--- ui/compress.go
+++ ui/compress.go
@@ -6,6 +6,9 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+// Compress is a middleware that compresses responds body.
+// The only difference of `github.com/go-chi/chi/v5/middleware#Compress` is the default value.
+// In order to be able to use this middleware, ensure to set url format with `github.com/go-chi/chi/v5/middleware#URLFormat`
 func Compress() func(next http.Handler) http.Handler {
 	return middleware.Compress(5, "text/html", "text/css", "text/javascript", "application/javascript",
 		"application/x-javascript", "application/json", "image/svg+xml", "image/vnd.microsoft.icon", "font/woff2", "application/wasm")
